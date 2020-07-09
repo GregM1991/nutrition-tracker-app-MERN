0b1336e8b9cbeb043e7093f7c7246f40cf6e8d27
Aaron Lewis <aaron.lewis1011@gmail.com>
2020-07-09 17:13:25 +1000

    more commits to break things

--- nutrition-tracker-backend/server.js
+++ nutrition-tracker-backend/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
-
+const connectDatabase = require('./config/db');
 const app = express();
 
-app.get('/', (req, res) => res.send('Backend Running'));
+connectDatabase();
+
+app.get('/', (req, res) => res.send('Backend Running oioioi'));
 
 const PORT = process.env.PORT || 3000;
 
