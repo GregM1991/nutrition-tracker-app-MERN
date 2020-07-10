647703571bc9bf07b1f826aadb031c6791ba962c
esthercsoke <esthercsoke@gmail.com>
2020-07-10 10:36:33 +1000

    added search bar

--- nutrition-tracker-frontend/src/App.js
+++ nutrition-tracker-frontend/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import axios from 'axios'
+import SearchBar from './SearchBar'
 
 const App = () => {
+    return (
+        <div className="ui container" style={{marginTop: '10px'}}>
+            <SearchBar />
+        </div>
+    )
   
 }
 
-export default App
+export default App;
