38b48907fa4de62e349b3272f841edadf8ed4046
Aaron Lewis <aaron.lewis1011@gmail.com>
2020-07-10 14:41:40 +1000

    added nutrition data routing to mongo

--- nutrition-tracker-backend/server.js
+++ nutrition-tracker-backend/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const connectDatabase = require('./config/db');
 const app = express();
+const cors = require('cors');
 
 connectDatabase();
 
 app.get('/', (req, res) => res.send('You hit the API Root'));
 
 app.use(express.json({ extended: false }));
-
+app.use(cors());
 app.use('/api/users', require('./routes/api/users'));
 
 const PORT = process.env.PORT || 3000;
--- nutrition-tracker-frontend/src/components/SearchController.js
+++ nutrition-tracker-frontend/src/components/SearchController.js
@@ -1,44 +1,72 @@
-import React from 'react'
-import SearchBar from './SearchBar'
-import nutritionRequest from '../utils/utils'
+import React from 'react';
+import SearchBar from './SearchBar';
+import nutritionRequest from '../utils/utils';
+import axios from 'axios';
 
+// user ID: 5f07bb66ca715df136270367
 
-
-class SearchController extends React.Component {  
+class SearchController extends React.Component {
   state = {
     searchValue: '',
     currentFood: '',
     currentNutritionData: ''
-  }
-
+  };
 
   handleSubmit = () => {
     nutritionRequest(this.state.searchValue)
-    .then((res) => {
-      this.setState({currentNutritionData: JSON.stringify(res.data.parsed[0].food.nutrients)})
-    })
-    .catch(err => {
-      console.log("somethings said no");
-      console.log(err);
-    })
-  }
+      .then((res) => {
+        this.setState({
+          currentNutritionData: JSON.stringify(
+            res.data.parsed[0].food.nutrients
+          )
+        });
+        return res;
+      })
+      .then((res) => {
+        const data = {
+          newMeal: {
+            name: this.state.searchValue,
+            calories: res.data.parsed[0].food.nutrients['ENERC_KCAL'],
+            energy: 0,
+            fat: res.data.parsed[0].food.nutrients['FAT'],
+            carbs: res.data.parsed[0].food.nutrients['CHOCDF'],
+            protein: res.data.parsed[0].food.nutrients['PROCNT']
+          }
+        };
 
+        axios
+          .put(
+            'http://localhost:3000/api/users/5f07bb66ca715df136270367',
+            JSON.stringify(data),
+            {
+              headers: { 'Content-Type': 'application/json' }
+            }
+          )
+          .then(() => {
+            console.log('ingredient has been posted');
+          });
+      })
+      .catch((err) => {
+        console.log('somethings said no');
+        console.log(err);
+      });
+  };
 
   render() {
-    return(
-      <div className="search">
-        <SearchBar 
+    return (
+      <div className='search'>
+        <SearchBar
           searchValue={this.state.searchValue}
           onSearchValueChange={(newSearchValue) => {
-            this.setState({searchValue: newSearchValue})
+            this.setState({ searchValue: newSearchValue });
           }}
           onEnter={this.handleSubmit}
         />
 
         {this.state.currentNutritionData}
       </div>
-    )
+    );
   }
 }
 
-export default SearchController
+export default SearchController;
