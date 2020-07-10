e7b614cef4b705e72b47c0c427b67ddabbcb608c
Greg Martin <gregm31@live.com>
2020-07-10 14:16:56 +1000

    Adding buttons

--- nutrition-tracker-frontend/src/App.js
+++ nutrition-tracker-frontend/src/App.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import Avatar from 'react-avatar';
-import SearchController from './components/SearchController'
+import React from "react";
+import Avatar from "react-avatar";
+import SearchController from "./components/SearchController";
 
 const App = () => {
   return (
     <div className="ui segment">
       <div className="ui two column very relaxed grid">
         <div class="column">
-          <Avatar githubHandle="mikedane" size={150} round="20px" />
-      </div>
-      <div className="column">
-          <div className="column ui container" style={{marginTop: '10px'}}>
-            <SearchController/>
+          <Avatar githubHandle="mikedane" size={100} round="20px" />
+          <p>Name goes here</p>
+        </div>
+        <div className="column">
+          <div className="column ui container" style={{ marginTop: "10px" }}>
+            <SearchController />
           </div>
+        </div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-
-export default App
+export default App;
--- nutrition-tracker-frontend/src/components/SearchController.js
+++ nutrition-tracker-frontend/src/components/SearchController.js
@@ -1,44 +1,47 @@
-import React from 'react'
-import SearchBar from './SearchBar'
-import nutritionRequest from '../utils/utils'
+import React from "react";
+import SearchBar from "./SearchBar";
+import nutritionRequest from "../utils/utils";
 
-
-
-class SearchController extends React.Component {  
+class SearchController extends React.Component {
   state = {
-    searchValue: '',
-    currentFood: '',
-    currentNutritionData: ''
-  }
-
+    searchValue: "",
+    currentFood: "",
+    currentNutritionData: "",
+  };
 
   handleSubmit = () => {
     nutritionRequest(this.state.searchValue)
-    .then((res) => {
-      this.setState({currentNutritionData: JSON.stringify(res.data.parsed[0].food.nutrients)})
-    })
-    .catch(err => {
-      console.log("somethings said no");
-      console.log(err);
-    })
-  }
-
+      .then((res) => {
+        this.setState({
+          currentNutritionData: JSON.stringify(
+            res.data.parsed[0].food.nutrients
+          ),
+        });
+      })
+      .catch((err) => {
+        console.log("somethings said no");
+        console.log(err);
+      });
+  };
 
   render() {
-    return(
-      <div className="search">
-        <SearchBar 
-          searchValue={this.state.searchValue}
-          onSearchValueChange={(newSearchValue) => {
-            this.setState({searchValue: newSearchValue})
-          }}
-          onEnter={this.handleSubmit}
-        />
+    return (
+      <div>
+        <button>Add Food</button>
+        <div className="search">
+          <SearchBar
+            searchValue={this.state.searchValue}
+            onSearchValueChange={(newSearchValue) => {
+              this.setState({ searchValue: newSearchValue });
+            }}
+            onEnter={this.handleSubmit}
+          />
 
-        {this.state.currentNutritionData}
+          {this.state.currentNutritionData}
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default SearchController
+export default SearchController;
