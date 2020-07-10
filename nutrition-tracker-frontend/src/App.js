96d0a5b56cf4b83f5c0ae96fc499fd3ece22ab3a
esthercsoke <esthercsoke@gmail.com>
2020-07-10 13:46:47 +1000

    added columns to app

--- nutrition-tracker-frontend/src/App.js
+++ nutrition-tracker-frontend/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import Avatar from 'react-avatar';
 import SearchController from './components/SearchController'
 
 const App = () => {
   return (
-      <div>
-          <div className="ui container" style={{marginTop: '10px'}}>
+    <div className="ui segment">
+      <div className="ui two column very relaxed grid">
+        <div class="column">
+          <Avatar githubHandle="mikedane" size={150} round="20px" />
+      </div>
+      <div className="column">
+          <div className="column ui container" style={{marginTop: '10px'}}>
             <SearchController/>
           </div>
       </div>
+    </div>
+    </div>
   )
 }
+
 
 export default App
---
+++ nutrition-tracker-frontend/src/IntakeContainer.js
@@ -0,0 +1,30 @@
+// import React from 'react';
+
+// const IntakeContainer = () => {
+
+//     return (
+//         <div>
+//             <table className="ui very basic collapsing celled table">
+//                 <thead>
+//                 <tr>
+//                     <th>Daily Intake</th>
+//                     <th>Total</th>
+//                 </tr></thead>
+//                 <tbody>
+//                     <tr>
+//                         <td>
+                        
+//                         </td>
+//                         <td>
+//                             hell
+//                         </td>
+//                     </tr>
+//                 </tbody>
+//             </table>
+//         </div>
+//     )
+// }
+
+
+
+// export default IntakeContainer
